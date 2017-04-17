72bb7168bd82a1dc08b1fef531b688670036cca8
Rosario Felix <elijan_11@hotmail.com>
2017-04-17 18:04:03 -0500

    eliminando y agregando text

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,7 @@ for(var i = 1 ; i <=4; i++){
   figura.classList.add("img-panda");
   var img = document.createElement("img");
   img.classList.add("img-box");
-
-
+  
 
   img.setAttribute("src", "assets/img/"+i+".jpg");
 
@@ -19,13 +18,28 @@ for(var i = 1 ; i <=4; i++){
  imagenes.appendChild(creandoDiv);
 }
 
-document.getElementById('imagenes').style.display="block";
-
+/*
 var quitar = document.getElementById("quitar");
 var close= document.getElementById('close');
 close.addEventListener("click",function(){
-   document.getElementsByTagName("img")[1].style.display="none";
-
+   document.getElementsByTagName("img")[1].style.display="none";*/
+//Poniendo imagenes
+var put = document.getElementById("put");
+put.addEventListener("click", function(){
+  document.getElementById("imagenes").style.display="block";
 });
 
-}
+//Implementando text
+
+var extincion = document.getElementById("extincion");
+extincion.addEventListener("click", function(){
+  document.getElementById("otro-text").style.visibility="hidden";
+});
+
+var origin = document.getElementById("origin");
+origin.addEventListener("click", function(){
+  document.getElementById("otro-text").style.visibility="visible";
+});
+
+
+};
