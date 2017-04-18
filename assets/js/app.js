1fc576649f6641f520ee259fa388ccf51dbec5d0
Rosario Felix <elijan_11@hotmail.com>
2017-04-18 11:03:12 -0500

    implementando cambios imagenes html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,35 @@
 window.onload=function(){
+//quitando imagenes
 
-var imagenes=document.getElementById('imagenes');
-
-for(var i = 1 ; i <=4; i++){
-  var creandoDiv = document.createElement("div");
-  creandoDiv.classList.add("grid");
-  var figura = document.createElement("figure");
-  figura.classList.add("img-panda");
-  var img = document.createElement("img");
-  img.classList.add("img-box");
-  var span = document.createElement("span");
-  span.innerHTML="X";
-  span.setAttribute("id","close")
-  img.setAttribute("src", "assets/img/"+i+".jpg");
-
-
-
- figura.appendChild(img);
- figura.appendChild(span);
- creandoDiv.appendChild(figura);
- imagenes.appendChild(creandoDiv);
-
-}
-var close = document.getElementById("close");
-close.addEventListener("click",function(){
-   document.getElementsByTagName("img")[0].style.display="none";
+var xUno = document.getElementById('close-uno').addEventListener("click",function() {
+  document.getElementById('imagen-panda').style.display="none";
 });
+var xDos = document.getElementById("close-dos").addEventListener("click",function() {
+  document.getElementById('imagen-panda-dos').style.display="none";
+});
+var xtres = document.getElementById('close-tres').addEventListener("click",function() {
+  document.getElementById('imagen-panda-tres').style.display="none";
+});
+var xcuatro= document.getElementById("close-cuatro").addEventListener("click",function() {
+  document.getElementById('imagen-panda-cuatro').style.display="none";
+});
+
 //Poniendo imagenes
 var put = document.getElementById("put");
 put.addEventListener("click", function(){
-  document.getElementById("imagenes").style.display="block";
+    document.getElementById('imagen-panda').style.display="inline-block";
+    document.getElementById('imagen-panda-dos').style.display="inline-block";
+    document.getElementById('imagen-panda-tres').style.display="inline-block";
+    document.getElementById('imagen-panda-cuatro').style.display="inline-block";
 });
-
 //Implementando text
-
-var extincion = document.getElementById("extincion");
-extincion.addEventListener("click", function(){
-  document.getElementById("otro-text").style.visibility="hidden";
+var other= document.getElementById("otro-text");
+var extincion = document.getElementById("extincion").addEventListener("click", function(e){
+other.classList.toggle("show-text");
 });
-
-var origin = document.getElementById("origin");
-origin.addEventListener("click", function(){
-  document.getElementById("otro-text").style.visibility="visible";
+var text =document.getElementById("text");
+var origin = document.getElementById("origin").addEventListener("click", function(e){
+  text.classList.toggle("show-text");
 });
-
 
 };
